e3210dff2efb81720273212dde8ecf71b8318356
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-07-21 16:56:34 +0530

    Add Merge Sort and Quick Sort

---
+++ includes/algos/merge_sort.h
@@ -0,0 +1,67 @@
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+#include <ds/dynamicarray.h>
+
+// Sorts and Merges to sub arrays of Array i.e. [start1,end1)+[start2,end2)
+template <typename T>
+void sort_and_merge_arrays(Array<T> *array, int start1, int end1, int start2, int end2, bool minSort = true)
+{
+    int index1 = start1;
+    int index2 = start2;
+    Array<T> *nArray = new Array<T>(end2 - start1);
+    for (int i = start1; i < end2; i++)
+    {
+        T val1 = array->get_element(index1);
+        T val2 = array->get_element(index2);
+        int index = i;
+        if (val1 != NULL && val2 != NULL)
+        {
+            if ((val1 < val2) == minSort)
+            {
+                index = index1;
+                index1 = (index1 + 1 >= end1) ? -1 : (index1 + 1);
+            }
+            else
+            {
+                index = index2;
+                index2 = (index2 + 1 >= end2) ? -1 : (index2 + 1);
+            }
+        }
+        else
+        {
+            if (val1 == NULL)
+            {
+                index = index2;
+                index2 = (index2 + 1 >= end2) ? -1 : (index2 + 1);
+            }
+            else
+            {
+                index = index1;
+                index1 = (index1 + 1 >= end1) ? -1 : (index1 + 1);
+            }
+        }
+        nArray->add_element(array->get_element(index));
+    }
+    for (int i = start1; i < end2; i++)
+    {
+        array->set_element(nArray->get_element(i - start1), i);
+    }
+    delete nArray;
+}
+
+// Sorts a Dynamic Array by using Merge Sort
+template <typename T>
+void merge_sort(Array<T> *array, int start, int end, bool minSort = true)
+{
+    if (end - 1 <= start)
+    {
+        return;
+    }
+    int m = end - ((end - start) / 2);
+    merge_sort(array, start, m, minSort);
+    merge_sort(array, m, end, minSort);
+    sort_and_merge_arrays(array, start, m, m, end, minSort);
+}
+
+#endif // MERGE_SORT_H
---
+++ includes/algos/quick_sort.h
@@ -0,0 +1,40 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+#include <ds/dynamicarray.h>
+
+// Partitions the Dynamic Array around a pivotand gives the pivot index
+template <typename T>
+int partition(Array<T> *array, int start, int end, bool minSort = true)
+{
+    T pivot = array->get_element(end - 1);
+    int i = start - 1;
+    for (int j = start; j < end; j++)
+    {
+        if (((array->get_element(j) < pivot) == minSort) && (array->get_element(j) != pivot))
+        {
+            i++;
+            T temp = array->get_element(i);
+            array->set_element(array->get_element(j), i);
+            array->set_element(temp, j);
+        }
+    }
+    T temp = array->get_element(i + 1);
+    array->set_element(pivot, i + 1);
+    array->set_element(temp, end - 1);
+    return i + 1;
+}
+
+// Sorts a Dynamic Array by using Quick Sort
+template <typename T>
+void quick_sort(Array<T> *array, int start, int end, bool minSort = true)
+{
+    if (start < end - 1)
+    {
+        int p = partition(array, start, end, minSort);
+        quick_sort(array, start, p, minSort);
+        quick_sort(array, p, end, minSort);
+    }
+}
+
+#endif // QUICK_SORT_H
